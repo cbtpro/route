082cd88b9fb89a289d5103c7e01a1efe480f6125
陈碧滔 <bitao.chen@envisioncn.com>
2017-03-16 18:13:27 +0800

    取消springboot的https设置,https设置应该在nginx里设置比较好

--- src/main/java/com/route/www/util/security/BCrypt.java
+++ src/main/java/com/route/www/util/security/BCrypt.java
@@ -11,8 +11,13 @@ public class BCrypt {
 	public static String encrypt(String password) {
 		return passwordEncoder.encode(password);
 	}
+	public static boolean dencrypt(String rawPassword, String encodedPassword) {
+		return passwordEncoder.matches(rawPassword,encodedPassword);
+	}
 	public static void main(String[] args) {
 		String password = BCrypt.encrypt("123456");
 		System.out.println(password);
+		boolean is = BCrypt.dencrypt("123456", "$2a$10$G547.2HOvOee5I9Nw9YUNO5I1KgMFsBdFVZqClySMpDJJqFJg49m2");
+		System.out.println(is);
 	}
 }
