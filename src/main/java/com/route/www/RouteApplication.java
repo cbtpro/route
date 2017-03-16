ed73e378e6ca8287f3b2a8c9e3aa8e8698a2122d
陈碧滔 <bitao.chen@envisioncn.com>
2017-03-16 15:34:26 +0800

    启用https，转发http到https

--- src/main/java/com/route/www/RouteApplication.java
+++ src/main/java/com/route/www/RouteApplication.java
@@ -1,9 +1,15 @@
 package com.route.www;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Bean;
 
 import com.route.www.util.SFTPSetting;
 
@@ -13,5 +19,32 @@ import com.route.www.util.SFTPSetting;
 public class RouteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RouteApplication.class, args);
+	}
+	
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+		return tomcat;
+	}
+
+	private Connector initiateHttpConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
 	}
 }
